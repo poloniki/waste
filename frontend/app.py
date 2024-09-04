3eca424c231c14f0d4ec519cf7753a620b639bcf
Nikita Polovinkin <poloniki@ya.ru>
2024-09-05 00:34:57 +0200

    w

--- frontend/app.py
+++ frontend/app.py
@@ -1,12 +1,12 @@
-import cv2
-import requests
-from image_prediction import create_image
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer, RTCConfiguration, WebRtcMode
 import av
+import requests
 import os
 from twilio.rest import Client
 
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
 account_sid = st.secrets["twilio"]["account_sid"]
 auth_token = st.secrets["twilio"]["auth_token"]
 client = Client(account_sid, auth_token)
